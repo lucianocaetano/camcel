e8a521e877e11104829b9808384c1bca1c545e8e
mauro caetano <mauroc@localhost.localdomain>
2024-06-20 08:59:30 -0300

    adapte las migraciones al nuevo mer que hiso luciano, pero me falta el LogDoc table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('rol', ["Admin", 'Portero', 'Empresa', "Empresa"])->default("Empresa");
             $table->timestamps();
         });
 
