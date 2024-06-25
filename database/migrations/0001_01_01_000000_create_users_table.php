d65233df0f21099bb6ff0fa513cf21477f9771e0
mauro caetano <mauroc@localhost.localdomain>
2024-06-25 10:02:05 -0300

    borre el atributo empresa que se repetia en rol

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->enum('rol', ["Admin", 'Portero', 'Empresa', "Empresa"])->default("Empresa");
+            $table->enum('rol', ["Admin", 'Portero', 'Empresa'])->default("Empresa");
             $table->timestamps();
         });
 
