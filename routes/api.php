faaff03fca9fd565d355359a86dde5ca5e0678ab
Mauro Caetano <mauro@localhost.localdomain>
2024-09-12 10:20:25 -0300

    agrege la ruta al api.php de los trabajos

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,17 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Admin\EnterpriseController as AdminEnterpriseController;
+use App\Http\Controllers\Admin\JobController as AdminJobController;
 use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:api')->group(function () {
     Route::get('/user', [AuthController::class, "me"]);
 
     Route::middleware([AdminMiddleware::class])->group(function () {
-        Route::get("/enterprises", [AdminEnterpriseController::class, "index"])->name("get-enterprises");
-        Route::get("/enterprises/{enterprise}", [AdminEnterpriseController::class, "show"])->name("get-enterprise");
-        Route::post("/enterprises", [AdminEnterpriseController::class, "store"])->name("store-enterprise");
-    })->name("dashboard-admin.");
+        Route::apiResource("/enterprises", AdminEnterpriseController::class);
+        Route::apiResource("/jobs", AdminJobController::class);
+    });
 });
 
 Route::middleware(["guest"])->prefix("auth/")->group(function () {
