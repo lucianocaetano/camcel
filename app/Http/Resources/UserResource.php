be394216c612133e28ca7caefdb1090955695e8e
Mauro Caetano <lucianocaetano1999@gmail.com>
2024-11-03 10:17:38 -0300

    Arregle el UserResource para que los roles esten en español

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource 
+class UserResource extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -17,7 +17,7 @@ class UserResource extends JsonResource
         return [
             "name" => $this->name,
             "email" => $this->email,
-            "rol" => $this->rol,
+            "rol" => $this->rol === "Enterprise"? ($this->enterprise ? ("Empresario"): ("Empresario sin Empresa")): ($this->rol === "Guard"? ("Guardia"): $this->rol),
             "id" => $this->id
         ];
     }
